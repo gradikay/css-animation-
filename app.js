ff812101ce7f95a072d8462fc8c7bfa45c3bdf74
GRADI M KAYAMBA <56418645+gradikay@users.noreply.github.com>
2019-11-17 10:07:07 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// AUTHOR: GRADI KAYAMBA
+// COPYRIGHT year loading bellow
+let copyRight = new Date().getFullYear();
+var copyR = document.getElementById("copy-right");
+copyR.innerHTML = (copyRight);
+//
